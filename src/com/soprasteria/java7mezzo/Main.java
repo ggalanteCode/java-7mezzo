60f3a5f30d74566167d42daf1c977c46a25abf64
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-06 16:41:28 +0200

    algoritmo per generare il mazzo di 40 carte

--- src/com/soprasteria/java7mezzo/Main.java
+++ src/com/soprasteria/java7mezzo/Main.java
@@ -1,10 +1,30 @@
 package com.soprasteria.java7mezzo;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random r = new Random();
+		int[] mazzo = new int[40];
+		int cartaEstratta;
+		for(int i = 0; i < mazzo.length; i++) {
+			while(true) {
+				boolean presente = false;
+				cartaEstratta = r.nextInt(40)+1;
+				for(int j = 0; j < 40; j++) {
+					if(cartaEstratta == mazzo[j]) {
+						presente = true;
+						break;
+					}
+				}
+				if(!presente) {
+					mazzo[i] = cartaEstratta;
+					break;
+				}
+			}
+		}
 	}
 
 }
